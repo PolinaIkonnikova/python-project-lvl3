5027a2ad3721cf8ee413124547c836a48901d725
Polina Ikonnikova <spollyv@gmail.com>
2022-08-20 01:29:31 +0500

    experiment with logs in terminal

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -6,24 +6,36 @@ from page_loader import create_parser, \
     download, user_friendly_message, traceback_message
 
 
+def read_logs(logs):
+    if logs:
+        with open('page_loader/aux/logs.log', 'r') as f:
+            print(f.read())
+
+
 def main(get_args=create_parser):
-    url, output_path = get_args()
+
+    url, output_path, logs = get_args()
+
     try:
         page_path = download(url, output_path)
         user_friendly_message('success loading', page_path)
         logger.info('The program was completed. Exit Code is 0.')
+        read_logs(logs)
         sys.exit(0)
     except CommonPageLoaderException:
         logger.warning('The program was completed. Exit Code is 1.')
+        read_logs(logs)
         sys.exit(1)
     except FileExistsError:
         user_friendly_message('dir exists')
         logger.warning('The program was completed. Exit Code is 2.')
+        read_logs(logs)
         sys.exit(2)
     except Exception as e:
         logger.warning(f'The unexpected error: {traceback_message(e)}\n'
                        'The program was completed. Exit Code is 1.')
         user_friendly_message('unexpected_err')
+        read_logs(logs)
         sys.exit(1)
 
 
